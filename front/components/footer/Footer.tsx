3153a4c87daaa471672725bde642a323a11cbe93
eiris elias <eiriselias@gmail.com>
2024-11-25 16:17:55 -0500

    se inicio la construcion del navbar y footer

---
+++ front/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-center py-4 font-bol text-white'>
+      <p>© FitHarMony Reservado todos los derechos - 2024</p>
+    </div>
+  )
+}
+
+export default Footer
